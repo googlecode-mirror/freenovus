64d4c1e6635679d1b0afb060cadf02598a33201a
tm8992 <tm8992@a961688e-bece-11dd-ab14-3be0eea8eb7e>
2008-11-30 12:03:38 +0000

    Added code to most files--rough.

--- trunk/index.php
+++ trunk/index.php
@@ -2,4 +2,19 @@
 /*
 And so it begins.
 */
+
+// Security setting
+define("IN_NOVUS", 1);
+
+// Get local information
+require("config.php");
+
+// (1) Include session management
+// (2) Include language based on session
+// (3) Process URL
+// (4) URL -> Info
+// (5) Info -> Skin
+// (6) Skin -> Out.
+
+
 ?>
